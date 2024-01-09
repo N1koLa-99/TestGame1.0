12a34c2c926c11c8d96a4ef1d2a5c09e3d2056b6
Nikola_K <140345347+N1koLa-99@users.noreply.github.com>
2024-01-09 22:01:53 +0200

    Create Roman.cs

---
+++ Code/Roman.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGame
+{
+    internal class Roman : Fighter
+    {
+        public Roman()
+        {
+            Blood = 280;
+            Attack = 70;
+        }
+        public override string FighterType
+        {
+            get
+            {
+                return "Римлянин";
+            }
+        }
+    }
+}
