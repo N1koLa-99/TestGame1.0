67744e2f4c19ab76e33c4842204a7f8322388ba1
Nikola_K <140345347+N1koLa-99@users.noreply.github.com>
2024-01-09 22:01:34 +0200

    Create Spartan.cs

---
+++ Code/Spartan.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGame
+{
+    internal class Spartan : Fighter
+    {
+        public Spartan()
+        {
+            Blood = 300;
+            Attack = 60;
+        }
+        public override string FighterType
+        {
+            get
+            {
+                return "Спартанец";
+            }
+        }
+    }
+}
