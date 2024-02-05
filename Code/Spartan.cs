3af3969b7ffd854db37f4bef96f26e1adae7a96c
Nikola_K <140345347+N1koLa-99@users.noreply.github.com>
2024-02-05 20:48:07 +0200

    Update Spartan.cs

--- Code/Spartan.cs
+++ Code/Spartan.cs
@@ -21,5 +21,9 @@ namespace TestGame
                 return "Спартанец";
             }
         }
+        public override int DamageChampion()
+        {
+            return Attack;
+        }
     }
 }
